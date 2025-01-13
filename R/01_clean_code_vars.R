e34616fe2a7c2a8376dc20c799c576d606a77f91
Nicole Fergestrom <nfergestrom@corevitas.com>
2025-01-13 09:22:43 -0600

    add new functions

---
+++ R/01_clean_code_vars.R
@@ -0,0 +1,34 @@
+#' clean_code_vars
+#' @description Function that cleans the "_code" variables by either removing the corresponding variable
+#' without the "_code" suffix (drop_dec_vars=TRUE option) or renaming the corresponding variable without
+#' the "_code" suffix by appending "_dec" and subsequently removes the "_code" suffix from the desired variables
+#'
+#' @param . current data frame
+#' @param drop_dec_vars Logical (TRUE or FALSE) value for whether to drop the description variables named without
+#' the "_code" suffix (TRUE) or rename them and keep them by appending "_dec" (FALSE). The default is TRUE.
+#'
+#' @return cleaned data frame with desired _code variables without the "_code" suffix
+#' @export
+#'
+#' @importFrom dplyr select rename_with %>%
+#' @importFrom stringr str_replace
+#' @importFrom tidyselect ends_with all_of any_of
+clean_code_vars <- function(., drop_dec_vars=TRUE){
+  curr_data <- .
+
+  code_vars <- gsub("_code", "", colnames(.)[which(grepl("_code", colnames(.)))])
+
+  if (drop_dec_vars==TRUE){
+    cleaned_data <- curr_data %>%
+      select(-any_of(code_vars)) %>%
+      rename_with(~str_replace(., "_code", ""), .cols = ends_with('_code'))
+  } else {
+    cleaned_data <- curr_data %>%
+      rename_with(~~str_c(., "_dec"), .cols = any_of(code_vars)) %>%
+      rename_with(~str_replace(., "_code", ""), .cols = ends_with('_code'))
+  }
+
+
+  return(cleaned_data)
+
+}
---
+++ R/03_create_dwh_formats.R
@@ -0,0 +1,52 @@
+#' create_dwh_formats
+#' @description Function that creates the format objects for labeling from the supplied data frame with expected variables
+#' including (dwh_lookup_acronym, dwh_lookup_code, dwh_lookup_value)
+#'
+#'
+#' @param dwh_lookup_dat supplied data frame with expected variables including (dwh_lookup_acronym, dwh_lookup_code, dwh_lookup_value).
+#' dwh_lookup_acronym should be the name of the format that ultimately gets new object stored to the working environment with "_f appended
+#' to the name. dwh_lookup_code is the numeric value and dwh_lookup_value is the value text to be used to label the corresponding numeric value.
+#'
+#' @return a new generated format object for each unique dwh_lookup_acronym with "_f" appended to the name (i.e. yes_no_only becomes yes_no_only_f
+#' stored in the R working environment)
+#' @export
+#'
+#' @importFrom stringr str_remove
+#' @importFrom dplyr filter mutate case_when distinct %>%
+#' @importFrom textclean replace_white
+create_dwh_formats <- function(dwh_lookup_dat){
+  dwh_lookups <- dwh_lookup_dat %>%
+    filter(! is.na(dwh_lookup_code)) %>%
+    mutate(dwh_lookup_acronym = case_when(column_name == "drug_category"           ~ "drug_category",
+                                          dwh_variable_name == "reason_1_category" ~ "drug_reason_codes_cat",
+                                          TRUE                                     ~ dwh_lookup_acronym)) %>%
+    distinct(dwh_lookup_acronym, dwh_lookup_code, dwh_lookup_value) %>%
+    # clean the formatting of of the code and value of the lookups
+    mutate(dwh_lookup_code = as.numeric(dwh_lookup_code),
+           dwh_lookup_value = textclean::replace_white(dwh_lookup_value),
+           dwh_lookup_value = gsub("'", " ", dwh_lookup_value)) %>%
+    mutate(dwh_lookup_value = gsub('\\"','', dwh_lookup_value)) %>%
+    mutate(dwh_lookup_value = gsub('\\\\','', dwh_lookup_value))
+
+  unique_lookups <- unique(dwh_lookups$dwh_lookup_acronym)
+
+  for (i in 1:length(unique_lookups)){
+    print(i)
+    print(unique_lookups[i])
+
+    curr_lookup <- unique_lookups[i]
+    curr_fomat_name <- paste(unique_lookups[i], "f", sep = "_")
+    curr_lookup_data <- dwh_lookups %>%
+      filter(dwh_lookup_acronym == curr_lookup) %>%
+      mutate(dwh_lookup_value = str_remove(dwh_lookup_value, '/$'))
+
+    curr_format <- paste("'", curr_lookup_data$dwh_lookup_value, "'", sep = "") %>%
+      paste(., "=", curr_lookup_data$dwh_lookup_code, collapse=",\n") %>%
+      paste("c(", ., ")", sep="") %>%
+      parse(text=.) %>%
+      eval()
+
+    assign(curr_fomat_name, curr_format, envir = parent.frame())
+    rm(curr_format)
+  }
+}
